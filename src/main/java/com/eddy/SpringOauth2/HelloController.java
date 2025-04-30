4fd524bd5841cfcdad97c748117e4c44092326c4
Eddy dashner <eddy.nzoabarinda@amalitech.com>
2025-04-30 21:08:15 +0200

    login with google

---
+++ src/main/java/com/eddy/SpringOauth2/HelloController.java
@@ -0,0 +1,13 @@
+package com.eddy.SpringOauth2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String greet(){
+        return "Welcome to Leave management system";
+    }
+}
